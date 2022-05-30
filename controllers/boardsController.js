eb2fb8bc00a38120a9745b6865d8912a6b204f15
Wallyson <wallysom2@gmail.com>
2022-05-30 16:29:45 -0300

    listando clientes

--- controllers/boardsController.js
+++ controllers/boardsController.js
@@ -11,42 +11,67 @@ export async function getAllCategories(req, res) {
 }
 
 export async function addCategory(req, res) {
-    const newCategory = req.body;
-    try {
-      const result = await db.query(`
+  const newCategory = req.body;
+  try {
+    const result = await db.query(`
         INSERT INTO categories (name)
         VALUES ($1);
       `, [req.body.name]);
-      
-      res.sendStatus(201);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send("Ocorreu um erro ao criar categoria!");
-    }
-  }
 
-  
-  export async function getAllGames(req, res) {
-    try {
-      const result = await db.query("SELECT * FROM games");
-      res.send(result.rows);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send("Ocorreu um erro ao obter os games!");
-    }
+    res.sendStatus(201);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Ocorreu um erro ao criar categoria!");
   }
+}
 
-  export async function addGame(req, res) {
-    const newGame = req.body;
-    try {
-      const result = await db.query(`
+
+export async function getAllGames(req, res) {
+  try {
+    const result = await db.query("SELECT * FROM games");
+    res.send(result.rows);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Ocorreu um erro ao obter os games!");
+  }
+}
+
+export async function addGame(req, res) {
+  const newGame = req.body;
+  try {
+    const result = await db.query(`
         INSERT INTO games ("name", "image", "stockTotal", "categoryId", "pricePerDay")
         VALUES ($1 , $2, $3, $4, $5);
       `, [req.body.name, req.body.image, req.body.stockTotal, req.body.categoryId, req.body.pricePerDay]);
-      
-      res.sendStatus(201);
-    } catch (e) {
-      console.log(e);
-      res.status(500).send("Ocorreu um erro ao criar jogo!");
-    }
+
+    res.sendStatus(201);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Ocorreu um erro ao criar jogo!");
   }
+}
+
+export async function getAllCustomers(req, res) {
+  try {
+    const result = await db.query("SELECT * FROM customers");
+    res.send(result.rows);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Ocorreu um erro ao obter os clientes!");
+  }
+}
+
+export async function addCustomer(req, res) {
+  const newGame = req.body;
+  try {
+    const result = await db.query(`
+        INSERT INTO customers ("name", "phone", "cpf", "birthday")
+        VALUES ($1 , $2, $3, $4);
+      `, [req.body.name, req.body.phone, req.body.cpf, req.body.birthday]);
+
+    res.sendStatus(201);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Ocorreu um erro ao criar novo cliente!");
+  }
+}
--- routes/boardRouter.js
+++ routes/boardRouter.js
@@ -4,6 +4,7 @@ import { getAllCategories} from "./../controllers/boardsController.js";
 import { addCategory } from "./../controllers/boardsController.js";
 import { getAllGames } from "./../controllers/boardsController.js";
 import { addGame } from "./../controllers/boardsController.js";
+import {getAllCustomers} from "./../controllers/boardsController.js";
 
 import {postCategoryMid} from "../middlewares/postCategoryMid.js";
 import {postGameMid} from "../middlewares/postGameMid.js";
@@ -15,6 +16,7 @@ boardRouter.get("/categories", getAllCategories);
 boardRouter.post("/categories", postCategoryMid, addCategory);
 boardRouter.get("/games", getAllGames);
 boardRouter.post("/games", postGameMid, addGame);
+boardRouter.get("/customers" , getAllCustomers);   
 
 
 export default boardRouter;
